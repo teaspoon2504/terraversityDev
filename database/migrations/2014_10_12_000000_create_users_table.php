f287e963461a3c86efb1c9056acad6c645f2d716
Ivan Radunovic <ivan@codingo.me>
2015-12-18 11:52:24 +0100

    Password nullable, needed for social logins.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique()->nullable();
-            $table->string('password', 60);
+            $table->string('password', 60)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
