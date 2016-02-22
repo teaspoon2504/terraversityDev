72869c7aeb6075aa52dc8e140ae79aae64baf583
Hernawan Fa'iz Abdillah <hernawan.faiz.a@mail.ugm.ac.id>
2016-02-22 20:29:51 +0700

    Add tables to manage Kupons

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,12 +12,13 @@ class AdminController extends Controller {
 
     public function getHome()
     {
-       // return view('panels.admin.home');
-    	// return view('user.dashboard');
+        // return view('panels.admin.home');
+        // return view('user.dashboard');
 
-    	$user=\Auth::user();
-        $kupons=\App\Kupon::all();
-        return view('panels.admin.home')->withUser($user)->withKupons($kupons);
+        $user = \Auth::user();
+        $pendingKupons = \App\Kupon::whereApproved(0)->get();
+        $approvedKupons = \App\Kupon::whereApproved(1)->get();
+        return view('panels.admin.home')->withUser($user)->withPendingKupons($pendingKupons)->withApprovedKupons($approvedKupons);
     }
 
 }
--- resources/views/panels/admin/home.blade.php
+++ resources/views/panels/admin/home.blade.php
@@ -9,26 +9,76 @@
 <h2>Admin Panel Home Page</h2>
 
 @if(!Auth::check())
-       selamat datang {{Auth::user()->first_name}}
-       <img src="{{Auth::user()->profile_picture}}">
+selamat datang {{Auth::user()->first_name}}
+<img src="{{Auth::user()->profile_picture}}">
 @endif
 
 
 @if (Auth::user()->email == 'eduterrajogja@terraversity.com')
-	@foreach (App\Kupon::all() as $kupon)
-		{{ $kupon->kode }} <br>
-	@endforeach
-	<a href="/admin/kupon/buat">buat kupon</a>
-
+  <div class="col-md-6">
+    <h3>Kupon Butuh Persetujuan</h3>
+    <table class="col-md-12">
+      <thead>
+        <tr>
+          <td>No.</td>
+          <td>Pengguna</td>
+          <td>Kode</td>
+          <td>Aksi</td>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($pending_kupons as $key => $kupon)
+        <tr>
+          <td>
+            {{ $key + 1 }}
+          </td>
+          <td>
+            {{ $kupon->user->first_name . ' ' . $kupon->user->last_name }} <br>
+          </td>
+          <td>
+            {{ $kupon->kode }} <br>
+          </td>
+          <td>
+            <a href="/admin/kupon/approve/{{ $kupon->kode }}" class="btn btn-primary">Setujui</a>
+          </td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+  <div class="col-md-6">
+    <h3>Kupon Telah Disetujui</h3>
+    <table class="col-md-12">
+      <thead>
+        <tr>
+          <td>No.</td>
+          <td>Pengguna</td>
+          <td>Kode</td>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($approved_kupons as $key => $kupon)
+        <tr>
+          <td>
+            {{ $key + 1 }}
+          </td>
+          <td>
+            {{ $kupon->user->first_name . ' ' . $kupon->user->last_name }} <br>
+          </td>
+          <td>
+            {{ $kupon->kode }} <br>
+          </td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
 @else
-<p>gak boleh lia</p>
-	<form action="kupon/subscribe" method="POST">
-		<input type="hidden" name="_token" value="{{ csrf_token() }}">
-		<input type="text" name="kode">
-		<input type="submit">
-	</form>
-
+  <p>gak boleh lia</p>
+  <form action="kupon/subscribe" method="POST">
+    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    <input type="text" name="kode">
+    <input type="submit">
+  </form>
 @endif
-
-
 @stop
