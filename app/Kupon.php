03236b384f0f3eb582eb79a899a24bcdc4af0ff3
Hernawan Fa'iz Abdillah <hernawan.faiz.a@mail.ugm.ac.id>
2016-02-22 20:27:22 +0700

    Add Kupon relationship to User

--- app/Kupon.php
+++ app/Kupon.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Kupon extends Model
 {
-    //
+    public function user()
+    {
+        return $this->hasOne('\App\Models\User', 'id', 'user_id');
+    }
+
 }
